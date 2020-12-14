f33d5246f25a8a183e2cece1db51d6134e998cd2
Danilo Lustosa <danilolustosa@gmail.com>
2020-12-14 15:44:42 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentChecked, AfterContentInit,
+  AfterViewChecked, AfterViewInit ,
+   DoCheck, OnChanges, OnDestroy, OnInit, Input, SimpleChanges
+  } from '@angular/core';
+  @Component({
+   selector: 'app-root',
+   styleUrls: ['./app.component.css'],
+   templateUrl: './app.component.html'
+  })
+  export class AppComponent implements OnChanges, OnInit, DoCheck,
+  AfterContentInit, AfterContentChecked,AfterViewInit,
+  AfterViewChecked,OnDestroy {
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'diretiva';
-}
+   displayChild: boolean=false;
+   constructor() {
+   console.log("AppComponent:Constructor");
+   }
+   toggle() {
+   this.displayChild=!this.displayChild;
+   }
+   ngOnChanges() {
+  67
+   console.log("AppComponent:OnChanges");
+   }
+   ngOnInit() {
+   console.log("AppComponent:OnInit");
+   }
+   ngDoCheck() {
+   console.log("AppComponent:DoCheck");
+   }
+   ngAfterContentInit() {
+   console.log("AppComponent:AfterContentInit");
+   }
+   ngAfterContentChecked() {
+   console.log("AppComponent:AfterContentChecked");
+   }
+   ngAfterViewInit() {
+   console.log("AppComponent:AfterViewInit");
+   }
+   ngAfterViewChecked() {
+   console.log("AppComponent:AfterViewChecked");
+   }
+   ngOnDestroy() {
+   console.log("AppComponent:OnDestroy");
+   }
+  }
